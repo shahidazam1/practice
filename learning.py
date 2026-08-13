f5cffdc61f245ebf73606d37cc500dff29525c42
Shahid <shahid.m@aifalabs.com>
2026-08-13 16:56:05 +0530

    practice basic revision

--- learning.py
+++ learning.py
@@ -1,5 +1,8 @@
 # lists
 
+from functools import reduce
+
+
 myList = [1, 2, 3, 4, 5]
 print(myList)
 
@@ -46,3 +49,104 @@ def my_function(x):
     return x + 1
 
 print(my_function(5))
+
+
+
+# Calculate:
+
+# sum
+# average
+# maximum
+# minimum
+numbers = [10, 20, 30, 40, 50]
+print("Calculations on the list of numbers [10, 20, 30, 40, 50]:")
+sum =0
+for i in numbers:
+    sum+=i
+print(f"Sum: {sum}")
+print(f"Average: {sum/len(numbers)}")
+
+# maximum
+max_number = 0
+for i in numbers:
+    if i > max_number:
+        max_number = i
+print(f"Maximum: {max_number}")
+
+# minimum
+min_number = numbers[0]
+for i in numbers:
+    if i < min_number:
+        min_number = i
+print(f"Minimum: {min_number}")
+
+# Day 2
+print("Day 2")
+
+myList = [1, 2, 3, 4, 5]
+print("List:", myList)
+updatedList = [i*i for i in myList if i % 2 == 0]
+print("Updated List:", updatedList)
+
+# lambda function
+
+sum_of_tow_numbers = lambda x,y: x+y
+print("Sum of 5 and 10 using lambda function:", sum_of_tow_numbers(5, 10))
+
+def func(*args):
+    for i in args:
+        print(i)
+    return "completed"
+val = func(1, 2, 3, 4, 5, "hello", "world")
+print(val, type(val))
+
+
+def func2(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+print(func2(name="Alice", age=30, city="New York"))
+
+
+def func3(*args, **kwargs):
+    for i in args:
+        print(i)
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+def func4(x, y):
+    print(f"x: {x}, y: {y}")
+    return x + y
+
+print("Sum of 5 and 10 using func4:", func4(y=10,x=5))
+
+mapList = [1, 2, 3, 4, 5]
+def mapSquare(x):
+ return x*x
+mappedlist = map(mapSquare, mapList)
+
+print ("Mapped List:", list(mappedlist))
+
+def func5(x):
+    return x==4
+
+filteredlist = filter(func5, mapList)
+print("Filtered List:", list(filteredlist))
+
+# filter example
+def is_even(x):
+    return x % 2 == 0
+
+even_numbers = list(filter(is_even, mapList))
+print("Even Numbers:", even_numbers)
+
+def sum(x, y):
+    return x + y 
+
+sum_result = reduce(sum, mapList)
+print("Sum of all numbers in mapList using reduce:", sum_result)
+
+# convert list to dictionary
+keys = ['a', 'b', 'c', 'd', 'e']
+values = [1, 2, 3, 4, 5]
+my_dict = dict(zip(keys, values))
+print("Dictionary:", my_dict)
